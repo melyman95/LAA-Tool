fde850ff5f3149f34a72d97632b9594e9afc4663
Matt <Matt@BUSTER>
2025-03-09 00:31:26 -0800

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace Large_Address_Aware__DotNETFramework_
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.filePathBox = new System.Windows.Forms.TextBox();
+            this.patchButton = new System.Windows.Forms.Button();
+            this.openButton = new System.Windows.Forms.Button();
+            this.backupCheckbox = new System.Windows.Forms.CheckBox();
+            this.restoreButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // filePathBox
+            // 
+            this.filePathBox.AllowDrop = true;
+            this.filePathBox.Location = new System.Drawing.Point(10, 377);
+            this.filePathBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.filePathBox.Name = "filePathBox";
+            this.filePathBox.Size = new System.Drawing.Size(681, 32);
+            this.filePathBox.TabIndex = 0;
+            this.filePathBox.TextChanged += new System.EventHandler(this.filePathBox_TextChanged);
+            this.filePathBox.DragDrop += new System.Windows.Forms.DragEventHandler(this.filePathBox_DragDrop);
+            // 
+            // patchButton
+            // 
+            this.patchButton.Location = new System.Drawing.Point(318, 443);
+            this.patchButton.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.patchButton.Name = "patchButton";
+            this.patchButton.Size = new System.Drawing.Size(172, 77);
+            this.patchButton.TabIndex = 2;
+            this.patchButton.Text = "Patch";
+            this.patchButton.UseVisualStyleBackColor = true;
+            this.patchButton.Click += new System.EventHandler(this.patchButton_Click);
+            // 
+            // openButton
+            // 
+            this.openButton.Location = new System.Drawing.Point(141, 443);
+            this.openButton.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.openButton.Name = "openButton";
+            this.openButton.Size = new System.Drawing.Size(172, 77);
+            this.openButton.TabIndex = 3;
+            this.openButton.Text = "Open File";
+            this.openButton.UseVisualStyleBackColor = true;
+            this.openButton.Click += new System.EventHandler(this.openButton_Click);
+            // 
+            // backupCheckbox
+            // 
+            this.backupCheckbox.AutoSize = true;
+            this.backupCheckbox.Location = new System.Drawing.Point(496, 492);
+            this.backupCheckbox.Name = "backupCheckbox";
+            this.backupCheckbox.Size = new System.Drawing.Size(174, 28);
+            this.backupCheckbox.TabIndex = 4;
+            this.backupCheckbox.Text = "Create Backup?";
+            this.backupCheckbox.UseVisualStyleBackColor = true;
+            // 
+            // restoreButton
+            // 
+            this.restoreButton.Location = new System.Drawing.Point(496, 424);
+            this.restoreButton.Name = "restoreButton";
+            this.restoreButton.Size = new System.Drawing.Size(174, 62);
+            this.restoreButton.TabIndex = 5;
+            this.restoreButton.Text = "Restore From Backup";
+            this.restoreButton.UseVisualStyleBackColor = true;
+            this.restoreButton.Click += new System.EventHandler(this.restoreButton_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Inherit;
+            this.AutoScroll = true;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(701, 548);
+            this.Controls.Add(this.restoreButton);
+            this.Controls.Add(this.backupCheckbox);
+            this.Controls.Add(this.openButton);
+            this.Controls.Add(this.patchButton);
+            this.Controls.Add(this.filePathBox);
+            this.Font = new System.Drawing.Font("Tahoma", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Name = "MainForm";
+            this.Text = "Large Address Aware";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.Resize += new System.EventHandler(this.MainForm_Resize);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox filePathBox;
+        private System.Windows.Forms.Button patchButton;
+        private System.Windows.Forms.Button openButton;
+        private System.Windows.Forms.CheckBox backupCheckbox;
+        private System.Windows.Forms.Button restoreButton;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,246 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+//using System.Linq;
+using System.Media;
+using System.Text;
+//using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Large_Address_Aware__DotNETFramework_
+{
+    public partial class MainForm: Form
+    {
+        const int IMAGE_FILE_LARGE_ADDRESS_AWARE = 0x20;
+        const int PE_HEADER_OFFSET_LOCATION = 0x3C;
+        const int CHARACTERISTICS_OFFSET = 0x16; // Offset in PE header
+        const int MACHINE_TYPE_OFFSET = 0x4; // Offset in PE header where machine type is stored
+        String initialDirectory = "";
+
+        public MainForm()
+        {
+            InitializeComponent();
+
+            filePathBox.AllowDrop = true;
+            filePathBox.Enabled = false;
+            restoreButton.Enabled = false;
+        }
+
+        private String searchForBackup(String path)
+        {
+            List<String> files = new List<String>(Directory.GetFileSystemEntries(path));
+            String returnPath = null;
+
+            foreach (var item in files)
+            {
+                if (Path.GetExtension(Path.GetFileName(item)) == ".BACK")
+                {
+                    returnPath = item;
+                    break;
+                }
+                else
+                {
+                    continue;
+                }
+            }
+            return returnPath;
+        }
+
+        private void createBackup(String filePath)
+        {
+            String newFilePath = Path.ChangeExtension(filePath, ".BACK");
+            if (!File.Exists(newFilePath))
+            {
+                File.Copy(filePath, newFilePath);
+            }
+        }
+
+        private void restoreFromBackup(String filePath)
+        {
+            String path = searchForBackup(Path.GetDirectoryName(filePath));
+            String newPath = Path.ChangeExtension(path, ".exe");
+            
+            if (!String.IsNullOrEmpty(path))
+            {
+                File.Copy(path, newPath, overwrite: true);
+                if (File.Exists(newPath))
+                {
+                    SystemSounds.Asterisk.Play();
+                    MessageBox.Show(Path.GetFileNameWithoutExtension(path) + " restored successfully!", "Success,", MessageBoxButtons.OK);
+                }
+                else
+                {
+                    SystemSounds.Exclamation.Play();
+                    MessageBox.Show(Path.GetFileNameWithoutExtension(path) + " could not be restored.", "Error,", MessageBoxButtons.OK);
+                }
+            }
+        }
+
+        private bool Is32Bit(ushort machineType)
+        {
+            bool is32 = false;
+
+            if (machineType == 0x14C)
+            {
+                is32 = true;
+            }
+
+            return is32;
+        }
+
+        private bool IsLAA()
+        {
+            bool laa = false;
+
+            return laa;
+        }
+
+        private void patchFile(String filePath)
+        {
+            try
+            {
+                if (backupCheckbox.Checked)
+                {
+                    createBackup(filePath);
+                }
+
+                FileStream fs = new FileStream(filePath, FileMode.Open, FileAccess.ReadWrite);
+                BinaryReader rdr = new BinaryReader(fs);
+                BinaryWriter wrtr = new BinaryWriter(fs);
+
+                if (fs.CanSeek == true)
+                {
+                    // Read PE header location
+                    fs.Seek(PE_HEADER_OFFSET_LOCATION, SeekOrigin.Begin);
+                    int PEHeaderOffset = rdr.ReadInt32();
+
+                    // Seek to the characteristics field
+                    fs.Seek(PEHeaderOffset + CHARACTERISTICS_OFFSET, SeekOrigin.Begin);
+                    ushort characteristics = rdr.ReadUInt16();
+
+                    // Seek to the Machine Type field
+                    fs.Seek(PEHeaderOffset + MACHINE_TYPE_OFFSET, SeekOrigin.Begin);
+                    ushort machineType = rdr.ReadUInt16();
+
+                    if (Is32Bit(machineType))
+                    {
+                        // Modify the Large Address Aware flag
+                        characteristics |= IMAGE_FILE_LARGE_ADDRESS_AWARE;
+
+                        // Write the modified flag back
+                        fs.Seek(PEHeaderOffset + CHARACTERISTICS_OFFSET, SeekOrigin.Begin);
+                        wrtr.Write(characteristics);
+                        SystemSounds.Asterisk.Play();
+                        String directoryPath = Path.GetDirectoryName(filePath);
+                        String folder = Path.GetFileName(directoryPath);
+                        MessageBox.Show(Path.GetFileName(filePathBox.Text) + " (" + folder + ")" + " should now be LAA! :) smileyface", "Congraturlations", MessageBoxButtons.OK);
+                    }
+                    else
+                    {
+                        SystemSounds.Exclamation.Play();
+                        MessageBox.Show("File is not 32 bit. Exiting...", "Error", MessageBoxButtons.OK);
+                        return;
+                    }
+                }
+                else
+                {
+                    SystemSounds.Exclamation.Play();
+                    MessageBox.Show("Something went wrong. Exiting...");
+                    return;
+                }
+            }
+            catch (System.IO.EndOfStreamException ex)
+            {
+                SystemSounds.Exclamation.Play();
+                MessageBox.Show(ex.ToString() + "!\n" + " this probably means you are attempting to use this tool on a 16-bit/DOS exe. Will only work on 32-bit Windows applications.", "Error", MessageBoxButtons.OK);
+            }
+        }
+
+        private void openButton_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fd = new OpenFileDialog();
+            if (String.IsNullOrEmpty(this.initialDirectory))
+            {
+                fd.InitialDirectory = AppDomain.CurrentDomain.BaseDirectory;
+            }
+            else
+            {
+                fd.InitialDirectory = this.initialDirectory;
+            }
+                fd.Filter = "Executable Files (*.exe)|*.exe";
+            fd.Title = "Select an exe";
+
+            if (fd.ShowDialog() == DialogResult.OK)
+            {
+                filePathBox.Text = fd.FileName;
+                this.initialDirectory = filePathBox.Text;
+                return;
+            }
+        }
+
+        private void patchButton_Click(object sender, EventArgs e)
+        {
+            if (File.Exists(filePathBox.Text.Trim()))
+            {
+                patchFile(filePathBox.Text.Trim());
+            }
+        }
+
+        private void filePathBox_TextChanged(object sender, EventArgs e)
+        {
+            String backup = searchForBackup(Path.GetDirectoryName(filePathBox.Text));
+            String extension = Path.GetExtension(backup);
+            if (extension == ".BACK")
+            {
+                restoreButton.Enabled = true;
+                return;
+            }
+            else
+            {
+                restoreButton.Enabled = false;
+                return;
+            }
+        }
+
+        private void MainForm_Resize(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            patchButton.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Right;
+            openButton.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left;
+            filePathBox.Anchor = AnchorStyles.Top | AnchorStyles.Right | AnchorStyles.Left;
+        }
+
+        private void filePathBox_DragDrop(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.Text))
+            {
+                filePathBox.Text = e.Data.GetData(DataFormats.Text).ToString();
+            }
+            else if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+                filePathBox.Text = string.Join(Environment.NewLine, files);
+            }
+        }
+
+        private void restoreButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                restoreFromBackup(filePathBox.Text);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("uh oh.\n" + ex.ToString());
+            }
+        }
+    }
+}
